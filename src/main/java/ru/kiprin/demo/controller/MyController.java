e262325dcade1b7ce67bbf1bd1db2d6576b50297
Kiprin Sergei <sergei@jinteki.dev>
2026-09-26 02:19:12 +0500

    added deps, changed port, created mycontroller

---
+++ src/main/java/ru/kiprin/demo/controller/MyController.java
@@ -0,0 +1,28 @@
+package ru.kiprin.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kiprin.demo.model.Request;
+import ru.kiprin.demo.model.Response;
+
+@RestController
+public class MyController {
+
+    @PostMapping(value = "/feedback")
+    public ResponseEntity<Response> feedback(@RequestBody Request request) {
+
+        Response response = Response.builder()
+                .uid(request.getUid())
+                .operationUid(request.getOperationUid())
+                .systemTime(request.getSystemTime())
+                .code("success")
+                .errorCode("")
+                .errorMessage("")
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/ru/kiprin/demo/model/Request.java
@@ -0,0 +1,22 @@
+package ru.kiprin.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Request {
+    private String uid;
+    private String operationUid;
+    private String systemName;
+    private String systemTime;
+    private String source;
+    private int communicationId;
+    private int templateId;
+    private int productCode;
+    private int smsCode;
+}
